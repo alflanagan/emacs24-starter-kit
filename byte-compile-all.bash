8fd196f81a6167b7e6a07db08d9d394734c0d69f
A. Lloyd Flanagan <a.lloyd.flanagan@gmail.com>
2014-12-03 15:21:15 -0500

    Possible NT fix for .elc files

--- byte-compile-all.bash
+++ byte-compile-all.bash
@@ -8,6 +8,10 @@ do
         #TODO: set up array of file names to skip
         if [[ $(basename "${ELFILE}") != ede-projects.el ]]; then
             emacs --batch -f batch-byte-compile "${ELFILE}"
+            if [[ ${OS} == Windows_NT ]]; then
+                #apparently required??
+                chmod u+x ${ELFILE}c
+            fi
         fi
     done
 done
