daff05476ac4238cc086e063539fb90ddb721341
A. Lloyd Flanagan <a.lloyd.flanagan@gmail.com>
2014-12-17 13:42:11 -0500

    Added error message on load of cedet-contrib

--- init.el
+++ init.el
@@ -13,7 +13,9 @@
     ;; IMPORTANT: Tou must place this *before* any CEDET component (including
     ;; EIEIO) gets activated by another package (Gnus, auth-source, ...).
     (load-file (expand-file-name "cedet-devel-load.el" cedet-home))
-    (load-file (expand-file-name "cedet-contrib-load.el" (expand-file-name "contrib" cedet-home)))))
+    (condition-case some-error
+	(load-file (expand-file-name "cedet-contrib-load.el" (expand-file-name "contrib" cedet-home)))
+      ((debug error) (message "[init] ERROR Probably need to re-compile contrib\ directory: %s" (cdr some-error))))))
 
 (require 'package)
 (add-to-list 'package-archives
